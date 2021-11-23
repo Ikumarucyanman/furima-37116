b524404195ffeea3d5db0d7d62906e62c671b2a4
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 00:16:08 +0900

    テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    name          {"いも"}
+    explanation   {"芋掘り"}
+    category_id   {"1"}
+    status_id     {"2"}
+    fee_id        {"3"}
+    prefecture_id {"4"}
+    schedule_id   {"5"}
+    price         {"10000"}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品投稿' do
+    it "nameがなければ投稿できない" do
+      @item.name = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Name can't be blank"
+    end
+    it "explanationがなければ投稿できない" do
+      @item.explanation = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Explanation can't be blank"
+    end
+    it "category_idがなければ投稿できない" do
+      @item.category_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Category can't be blank"
+    end
+    it "status_idがなければ投稿できない" do
+      @item.status_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Status can't be blank"
+    end
+    it "fee_idがなければ投稿できない" do
+      @item.fee_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Fee can't be blank"
+    end
+    it "prefecture_idがなければ投稿できない" do
+      @item.prefecture_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+    end
+    it "schedule_idがなければ投稿できない" do
+      @item.schedule_id = ""
+      @item.valid?
+      expect(@item.errors.full_messages).to include "Schedule can't be blank"
+    end
+    it "priceがなければ投稿できない" do
+      @item.price = ""
+      @item.valid?
+      # binding.pry
+      expect(@item.errors.full_messages).to include "Price can't be blank"
+    end
+  end
 end
