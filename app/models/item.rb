dc3ea2dd0a4f988c36d4c1e81e99c68a90813d97
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-23 15:52:47 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :schedule
 
   validates :name,          presence: true
   validates :explanation,   presence: true
-  validates :category_id,   presence: true
-  validates :status_id,     presence: true
-  validates :fee_id,        presence: true
-  validates :prefecture_id, presence: true
-  validates :schedule_id,   presence: true
+  validates :category_id,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id,     numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id,        numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :schedule_id,   numericality: { other_than: 1 , message: "can't be blank"}
   varidates :price,         presence: true
   validates :user,          presence: true
 
