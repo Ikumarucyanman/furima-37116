8b4dfc4feb29f41c3d06809b8286a6e5b3e0b1af
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 12:45:05 +0900

    投稿ができる場合のテストコードOK

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,11 +2,12 @@ FactoryBot.define do
   factory :item do
     name          {"いも"}
     explanation   {"芋掘り"}
-    category_id   {"1"}
+    category_id   {"2"}
     status_id     {"2"}
     fee_id        {"3"}
     prefecture_id {"4"}
     schedule_id   {"5"}
     price         {"10000"}
+    user_id       {"6"}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,46 +6,52 @@ RSpec.describe Item, type: :model do
   end
   
   describe '商品投稿' do
-    it "nameがなければ投稿できない" do
-      @item.name = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Name can't be blank"
+    context "投稿ができる場合" do
+      it "name、explanation、category_id、status_id、fee_id、prefecture_id、schedule_id、price、user_idがない場合投稿できない" do
+        expect(@item).to be_valid
+      end
     end
-    it "explanationがなければ投稿できない" do
-      @item.explanation = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Explanation can't be blank"
-    end
-    it "category_idがなければ投稿できない" do
-      @item.category_id = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Category can't be blank"
-    end
-    it "status_idがなければ投稿できない" do
-      @item.status_id = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Status can't be blank"
-    end
-    it "fee_idがなければ投稿できない" do
-      @item.fee_id = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Fee can't be blank"
-    end
-    it "prefecture_idがなければ投稿できない" do
-      @item.prefecture_id = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Prefecture can't be blank"
-    end
-    it "schedule_idがなければ投稿できない" do
-      @item.schedule_id = ""
-      @item.valid?
-      expect(@item.errors.full_messages).to include "Schedule can't be blank"
-    end
-    it "priceがなければ投稿できない" do
-      @item.price = ""
-      @item.valid?
-      # binding.pry
-      expect(@item.errors.full_messages).to include "Price can't be blank"
+    context "投稿ができない場合" do
+      it "nameがなければ投稿できない" do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it "explanationがなければ投稿できない" do
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Explanation can't be blank"
+      end
+      it "category_idがなければ投稿できない" do
+        @item.category_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it "status_idがなければ投稿できない" do
+        @item.status_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Status can't be blank"
+      end
+      it "fee_idがなければ投稿できない" do
+        @item.fee_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Fee can't be blank"
+      end
+      it "prefecture_idがなければ投稿できない" do
+        @item.prefecture_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+      it "schedule_idがなければ投稿できない" do
+        @item.schedule_id = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Schedule can't be blank"
+      end
+      it "priceがなければ投稿できない" do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
     end
   end
 end
