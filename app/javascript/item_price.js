b84a52e046fc09fdbdf715d03c04286baea1b7a8
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 20:31:06 +0900

    JavaScript完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,10 +1,11 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  const priceContent1 = document.getElementById("add-tax-price");
-  const priceContent2 = document.getElementById("profit");
   
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
+    const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = inputValue * 0.1;
+    const priceProfit = document.getElementById("profit");
+      priceProfit.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
   })
 });
