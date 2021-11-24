12c0a9e6f4facec95ab48c8122f8182c322dd06f
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 18:23:38 +0900

    JavaScriptの要素取得

---
+++ app/javascript/item_price.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+  const priceContent1 = document.getElementById("add-tax-price");
+  const priceContent2 = document.getElementById("profit");
+  console.log(priceInput);
+});
