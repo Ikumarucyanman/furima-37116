1a7e9c55fd1a9bd0c2cb0ecf646f288fa0d72be6
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 19:13:33 +0900

    value属性取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,10 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
-  })
   const priceContent1 = document.getElementById("add-tax-price");
   const priceContent2 = document.getElementById("profit");
   
-  // priceInput.addEventListener("input", () => {
-  //   console.log("イベント発火");
-  // })
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+  })
 });
