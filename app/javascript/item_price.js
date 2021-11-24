eb8cdbaf4fd9999712597085e15d97577dbcc498
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 19:06:16 +0900

    発火完了

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
-  const priceContent1 = document.getElementById("add-tax-price");
-  const priceContent2 = document.getElementById("profit");
-  
   priceInput.addEventListener("input", () => {
     console.log("イベント発火");
   })
+  const priceContent1 = document.getElementById("add-tax-price");
+  const priceContent2 = document.getElementById("profit");
+  
+  // priceInput.addEventListener("input", () => {
+  //   console.log("イベント発火");
+  // })
 });
