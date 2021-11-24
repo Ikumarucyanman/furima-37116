e802feb32ee45da2958013ac9d33a5bb197a52ca
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-24 18:36:57 +0900

    イベント発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,5 +2,8 @@ window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
   const priceContent1 = document.getElementById("add-tax-price");
   const priceContent2 = document.getElementById("profit");
-  console.log(priceInput);
+  
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
 });
