61dabe7fc1ec11ec24957c9d092cd79da8546998
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-26 18:28:18 +0900

    editアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,10 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :fee_id, :prefecture_id, :schedule_id, :price).merge(user_id: current_user.id)
