1861e89b9fcc596f3f0e07a02b57a0a55c3edc9f
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-28 19:22:31 +0900

    保存する処理完了

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -11,4 +11,10 @@ class OrderShipping
     validates :phone_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    shipping = Shipping.create(postal_code: postal_code, prefecture: prefecture, city: city, addresses: addresses, building: building, phone_number: phone_number, order_id: order.id)
+  end
 end
