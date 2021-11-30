307246258ea9dc371c492811bb8f1143e7c8a550
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-30 12:07:09 +0900

    鍵情報を環境変数として設定完了

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_8d78d1f9425f3c1a7f9392d7");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
@@ -22,10 +22,10 @@ const pay = () => {
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
-      document.getElementById("order_shipping_number").removeAttribute("name");
-      document.getElementById("order_shipping_exp_month").removeAttribute("name");
-      document.getElementById("order_shipping_exp_year").removeAttribute("name");
-      document.getElementById("order_shipping_cvc").removeAttribute("name");
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,6 +1,6 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
@@ -9,6 +9,7 @@ class OrderShipping
     validates :city
     validates :addresses
     validates :phone_number, format: {with: /\A[0-9]{11}\z/ }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
 
