a74595d65775abf884a17df5cad0d14d9eb10afa
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-11-30 17:49:29 +0900

    購入後の商品、編集、削除、購入ボタン非表示

--- spec/models/order_shipping_spec.rb
+++ spec/models/order_shipping_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe OrderShipping, type: :model do
     before do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      @order_shipping = FactoryBot.build(:order_shipping, user_id: user.id)
+      @order_shipping = FactoryBot.build(:order_shipping, user_id: user.id, item_id: item.id)
     end
   
     context '内容に問題ない場合' do
