d0f4f1e2f97fcb47ed461d8ad1c9cb015fd8d65d
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-12-01 11:38:40 +0900

    token文字削除完了

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,7 +18,8 @@ const pay = () => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token'>`;
+        // const tokenObj = `<input value=${token} name='token'>`;
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
 
