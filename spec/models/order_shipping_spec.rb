cc0826fa231ec6409ea3774e986bb1c854af07e3
Ikumarucyanman <92787585+Ikumarucyanman@users.noreply.github.com>
2021-12-01 12:17:35 +0900

    テストコード記述変更

--- spec/models/order_shipping_spec.rb
+++ spec/models/order_shipping_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe OrderShipping, type: :model do
         @order_shipping.valid?
         expect(@order_shipping.errors.full_messages).to include "Token can't be blank"
       end
-      it "電話番号は、10桁以上11桁以内の半角数値のみ保存可能なこと" do
+      it "電話番号は、10桁か11桁の半角数値のみ保存可能なこと" do
         @order_shipping.phone_number = "090123456789"
         @order_shipping.valid?
         expect(@order_shipping.errors.full_messages).to include "Phone number is invalid"
